d7e75771cfc814f60ef39097ad044db53ba4ae84
Ademola Adegbuyi <ooade96@gmail.com>
2017-09-08 21:05:21 +0100

    Fixed Instagram text

--- components/contact.vue
+++ components/contact.vue
@@ -4,7 +4,7 @@
 		<a target="_blank" href="https://twitter.com/_ooade"> Twitter </a>
 		<a target="_blank" href="https://linkedin.com/in/ooade"> LinkedIn </a>
 		<a target="_blank" href="https://facebook.com/ademola.o.adegbuyi"> Facebook </a>
-		<a target="_blank" href="https://instagram.com/_ooade"> Intagram </a>
+		<a target="_blank" href="https://instagram.com/_ooade"> Instagram </a>
 	</div>
 </template>
 
