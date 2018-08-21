edc68806110604f7d00744604dd0efa82d1a2847
Ademola Adegbuyi <ooade96@gmail.com>
2018-08-21 12:07:28 +0100

    switch to workbox

--- next.config.js
+++ next.config.js
@@ -1,44 +1,35 @@
 const path = require('path');
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
-	webpack: (config, { dev }) => {
+	webpack: (config, { buildId, dev }) => {
 
 		/**
 		 * Install and Update our Service worker
 		 * on our main entry file :)
 		 * Reason: https://github.com/ooade/NextSimpleStarter/issues/32
 		 */
+
 		const oldEntry = config.entry;
 
 		config.entry = () =>
 			oldEntry().then(entry => {
-				entry['main.js'] && entry['main.js'].push(path.resolve('./src/offline'));
+				entry['main.js'] &&
+					entry['main.js'].push(path.resolve('./src/offline'));
 				return entry;
 			});
 
 		/* Enable only in Production */
 		if (!dev) {
 			// Service Worker
+
 			config.plugins.push(
-				new SWPrecacheWebpackPlugin({
-					cacheId: 'next-ss',
-					filepath: './static/sw.js',
-					minify: true,
-					staticFileGlobsIgnorePatterns: [/\.next\//],
-					staticFileGlobs: [
-						'static/**/*' // Precache all static files by default
-					],
-					runtimeCaching: [
-						// Example with different handlers
-						{
-							handler: 'fastest',
-							urlPattern: /[.](png|jpg|css)/
-						},
-						{
-							handler: 'networkFirst',
-							urlPattern: /^http.*/ //cache all files
-						}
+				new WorkboxPlugin.InjectManifest({
+					swSrc: path.join(__dirname, 'src', 'sw.js'),
+					swDest: path.join(__dirname, '.next', 'sw.js'),
+					globDirectory: __dirname,
+					globPatterns: [
+						'static/**/*.{png,jpg,ico}' // Precache all static assets by default
 					]
 				})
 			);
---
+++ src/sw.js
@@ -0,0 +1,40 @@
+workbox.core.setCacheNameDetails({ prefix: 'next-ss' });
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+workbox.precaching.suppressWarnings();
+
+/**
+ * Ignore the non-important files added as a result of
+ * webpack's publicPath thingy, for now...
+ */
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+workbox.precaching.precacheAndRoute(
+	self.__precacheManifest.filter(
+		m =>
+			!m.url.startsWith('bundles/') &&
+			!m.url.startsWith('static/commons') &&
+			m.url !== 'build-manifest.json'
+	),
+	{}
+);
+
+workbox.routing.registerRoute(
+	/[.](png|jpg|css)/,
+	workbox.strategies.cacheFirst({
+		cacheName: 'assets-cache',
+		cacheableResponse: {
+			statuses: [0, 200]
+		}
+	}),
+	'GET'
+);
+
+workbox.routing.registerRoute(
+	/^http.*/,
+	workbox.strategies.cacheFirst({
+		cacheName: 'http-cache'
+	}),
+	'GET'
+);
